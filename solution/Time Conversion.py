1046414519517628d56c6c651cec3597736012c7
Nimit Trevadiya <68644815+nimit0703@users.noreply.github.com>
2022-01-08 00:32:28 +0530

    Create Time Conversion.py

---
+++ solution/Time Conversion.py
@@ -0,0 +1,10 @@
+#https://www.hackerrank.com/challenges/time-conversion/problem?isFullScreen=false
+
+s=input()
+time=s[8:]
+if time=='PM':
+    a=12+int(s[:2])
+    s1=str(a)+s[2:8]
+    print(s1[:8])
+else:
+    print(s[:8])
